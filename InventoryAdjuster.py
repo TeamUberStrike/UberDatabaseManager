0239194283d9dd50cd5711a105508ceb74dce268
xVizier <143770098+xVizier@users.noreply.github.com>
2023-09-19 20:27:56 -0400

    Update InventoryAdjuster.py

--- InventoryAdjuster.py
+++ InventoryAdjuster.py
@@ -12,9 +12,13 @@ BetaHeroShirt = 1038
 GlobalModShirt = 1040
 ModShirt = 1041
 QAShirt = 1042
-
+SLDShirt = 1044
+DEDShirt = 1045
+C4CShirt = 1046
+ST6IXShirt = 1047
+#Admins [1, 530], SeniorModerator [3, 38], SeniorQA [39], Moderator [2, 4], QA [5, 6, 7, 9, 30, 105]
 Cmids = [1]
-ItemstoAdd = [AdminShirt, QAShirt, BetaHeroShirt]
+ItemstoAdd = [AdminShirt]
 
 CmidRange = len(list(Cmids))
 
@@ -23,14 +27,14 @@ for i in Cmids:
     for Item in ItemstoAdd:
         ItemId = Item
         print("Item:",ItemId)
-        db.Test.update_one(
+        db.TestUser.update_one(
             {"UserId": i},
             { "$addToSet": { "Inventory" : 
                 {
                     "AmountRemaining": -1,
                     "Cmid": i,
                     "ExpirationDate": None,
-                    "ItemId": Item
+                    "ItemId": ItemId
                 }
             }}
         )
