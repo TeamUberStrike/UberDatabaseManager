eb5fca6c7f2022d28cfaad1ffcc33f6d77accef5
xVizier <143770098+xVizier@users.noreply.github.com>
2023-09-22 11:07:30 -0400

    updating scripts

--- PointsUpdater.py
+++ PointsUpdater.py
@@ -2,49 +2,51 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb://uber:admin@localhost:27017/?authMechanism=SCRAM-SHA-1&authSource=admin")
 db = client.admin
-collection = db["FixedUsers"]
-collection2 = db["BrokenUsers"]
-collection3 = db["Users"]
+CollectionFixedUsers = db["FixedUsers"]
+CollectionBrokenUsers = db["BrokenUsers"]
+CollectionCurrentUsers = db["Users"]
 
 result = db.Users.find()
 total_accounts=len(list(result))
-AwardWinners = [2, 5, 13, 69, 71, 114, 118, 140, 224, 491, 555, 670, 815]
-
-for i in AwardWinners:
-    print("Cmid:",i)
-    BrokenDB = collection2.find_one({"UserId": i,})
-    FixedDB = collection.find_one({"UserId": i})
-    FixedPointsCount = FixedDB["Wallet"]["Points"]
-    BrokenPointsCount = BrokenDB["Wallet"]["Points"]
-    
-    AwardPoints = BrokenPointsCount + 50000
-
-    db.BrokenUsers.update_one(
-        {"UserId": i},
-        { "$set": {"Wallet.Points": AwardPoints}}
-    )
+AwardWinners = [2, 5, 13, 69, 71, 114, 118, 140, 224, 491, 555, 670, 815, 883]
 
 for j in range(1,total_accounts+1):
     print("Cmid:",j)
     if j >= 1 and j <= 312 or j >= 314 and j <= 504 or j >= 506 and j <= 862:
-        BrokenDB = collection2.find_one({"UserId": j,})
-        FixedDB = collection.find_one({"UserId": j})
-        CurrentDB = collection3.find_one({"UserId": j})
+        BrokenDB = CollectionBrokenUsers.find_one({"UserId": j,})
+        FixedDB = CollectionFixedUsers.find_one({"UserId": j})
+        CurrentDB = CollectionCurrentUsers.find_one({"UserId": j})
         FixedPointsCount = FixedDB["Wallet"]["Points"]
         BrokenPointsCount = BrokenDB["Wallet"]["Points"]
         CurrentPointsCount = CurrentDB["Wallet"]["Points"]
         FixedKillsCount = FixedDB["Kills"]
         BrokenKillsCount = BrokenDB["Kills"]
         CurrentKillsCount = CurrentDB["Kills"]
-     
+    
         NewPointsCount = BrokenPointsCount - FixedPointsCount
         Kills = FixedKillsCount - BrokenKillsCount
         NewBalance = Kills * 10
         NewBalance = NewBalance + CurrentPointsCount
-        
-        db.TestUser.update_one(
+        for i in AwardWinners:
+            NewBalance = NewBalance + 50000
+    
+        db.Users.update_one(
             { "UserId": j},
             { "$set": {"Wallet.Points": NewBalance}}
         )
-    
+    if j >= 863:
+        FixedDB = CollectionFixedUsers.find_one({"UserId": j})
+        CurrentDB = CollectionCurrentUsers.find_one({"UserId": j})
+        CurrentPointsCount = CurrentDB["Wallet"]["Points"]
+        CurrentKillsCount = CurrentDB["Kills"]
+        NewBalance = Kills * 10
+        NewBalance = NewBalance + CurrentPointsCount
+        db.Users.update_one(
+            { "UserId": j},
+            { "$set": {"Wallet.Points": NewBalance}}
+        )
+        
+
+
+
     
